7063305e21c2fbe1f6b29802fee36b2b812a4d73
opsb <oliver@opsb.co.uk>
2013-05-20 15:14:03 +0200

    Allow capybara to operate without app

--- lib/capybara/mechanize/driver.rb
+++ lib/capybara/mechanize/driver.rb
@@ -3,9 +3,11 @@ require 'capybara/mechanize/browser'
 class Capybara::Mechanize::Driver < Capybara::RackTest::Driver
   
   def initialize(app, options = {})
-    raise ArgumentError, "mechanize requires a rack application, but none was given" unless app
-
-    super
+    if Capybara.run_server && !app
+      raise ArgumentError, "mechanize requires a rack application, but none was given"
+    end
+    @app = app
+    @options = DEFAULT_OPTIONS.merge(options)
   end
 
   def remote?(url)
